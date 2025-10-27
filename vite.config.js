eb783e9e9b460e4a239b6162f57e39203d6c0bbb
MyTHOMASTHOMAS <thomasthomasz2xz@gmail.com>
2025-10-27 22:37:30 +0400

    CORRECTION: correction chemin incorrect\nFEAT: Ajout icone

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 export default defineConfig({
+    base: '/Portfolio/',
     plugins: [
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/icon.svg', // Le chemin vers votre fichier source
+                    dest: 'assets' // Le dossier de destination dans "dist"
+                }
+            ]
+        })
     ],
 })
